3a58a7359d9839f384a504df97e5822e24728c5d
2200090246 <148328823+2200090246@users.noreply.github.com>
2024-12-11 13:37:36 +0530

    Update Login.js

--- eduerp-frontend/src/components/Login.js
+++ eduerp-frontend/src/components/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
 
     const handleLogin = async () => {
         try {
-            const response = await axios.post("http://localhost:8080/api/auth/login", {
+            const response = await axios.post("https://springbootsdpbackend.up.railway.app/api/auth/login", {
                 username,
                 password,
                 role,
